60704dc523d3d272d228e63eb950e950dfa051ca
babak zarrinbal <babak.zarrinbal@gmail.com>
2019-08-28 11:35:37 +0430

    added queues to export and cachedlimit improvment

--- index.js
+++ index.js
@@ -5,55 +5,66 @@ var exp = {
   retry: 0,
   retrydelay: 0,
   cachetime: 0,
-  cachelength: 0
+  cachelength: 0,
+  inqueue: [],
+  processing: [],
+  resolved: []
 };
-var inqueue = [],
-  processing = [],
-  resolved = [];
-
+var clearresolvedtimout = () => {
+  if (exp.cachetime && exp.resolved.length)
+    exp.resolved = exp.resolved.filter(
+      r => r.time + exp.cachetime <= Date.now()
+    );
+};
 exp.push = function(...input) {
   let inputid = JSON.stringify(input);
-  let prev;
-  if (exp.cachetime || exp.cachelength) {
-    if (exp.cachetime && resolved.length)
-      resolved = resolved.filter(r => r.time + exp.cachetime <= Date.now());
-    prev = resolved.find(itm => itm.inputid == inputid);
-    if (prev) return prev.result;
-  }
-  prev = processing.find(itm => itm.inputid == inputid);
-  if (prev) return prev.result;
 
-  prev = inqueue.find(itm => itm.inputid == inputid);
-  if (prev) return prev.result;
+  // check previous queues for this inputid
+  let prev;
+  let checkqs = ["processing", "inqueue"];
+  if (exp.cachetime || exp.cachelength) {
+    clearresolvedtimout();
+    checkqs.unshift("resolved");
+  }
+  for (let q of checkqs) {
+    prev = exp[q].find(itm => itm.inputid == inputid);
+    if (prev) {
+      prev.used++;
+      return prev.result;
+    }
+  }
+
+  // define new task
   let resolve, reject;
   let result = new Promise((res, rej) => {
     resolve = res;
     reject = rej;
   });
-  inqueue.push({
+  exp.inqueue.push({
     input,
     inputid,
     result,
     resolve,
     reject,
     time: Date.now(),
-    retry: 0
+    retry: 0,
+    used: 0
   });
   tick();
   return result;
 };
 
 var tick = async function() {
-  if (!inqueue.length || (exp.threads && processing.length == exp.threads)) {
-    if (exp.cachetime && resolved.length)
-      resolved = resolved.filter(r => r.time + exp.cachetime <= Date.now());
+  if (
+    !exp.inqueue.length ||
+    (exp.threads && exp.processing.length == exp.threads)
+  )
     return;
-  }
 
   let result,
     error,
-    newjob = inqueue.shift();
-  processing.push(newjob);
+    newjob = exp.inqueue.shift();
+  exp.processing.push(newjob);
   if (exp.timeout) {
     let pres = await Promise.race([
       new Promise(async res => {
@@ -79,20 +90,13 @@ var tick = async function() {
     }
   }
 
-  let donejobindex = processing.findIndex(p => p.inputid == newjob.inputid);
-  let donejob = processing[donejobindex];
-  processing = donejobindex
-    ? [
-        ...processing.slice(0, donejobindex - 1),
-        ...processing.slice(donejobindex)
-      ]
-    : processing.slice(1);
-
+  let donejob = exp.processing.find(p => p.inputid == newjob.inputid);
+  exp.processing = exp.processing.filter(pj => pj.inputid != donejob.inputid);
   if (error) {
     if (exp.retry && exp.retry > newjob.retry) {
       newjob.retry++;
       return setTimeout(() => {
-        inqueue.push(newjob);
+        exp.inqueue.push(newjob);
         tick();
       }, exp.retrydelay || 0);
     } else {
@@ -102,13 +106,13 @@ var tick = async function() {
     newjob.resolve(result);
   }
 
-  if (exp.cachetime && resolved.length)
-    resolved = resolved.filter(r => r.time + exp.cachetime <= Date.now());
+  clearresolvedtimout();
 
   if (exp.cachelength) {
-    if (resolved.length >= exp.cachelength) {
-      resolved.shift();
-      resolved.push(donejob);
+    if (exp.resolved.length >= exp.cachelength) {
+      exp.resolved.sort((a, b) => (a.used > b.used ? 1 : -1));
+      exp.resolved.pop();
+      exp.resolved.unshift(donejob);
     }
   }
   tick();
